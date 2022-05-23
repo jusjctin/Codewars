6e2b6f2a72d952dd281dc063e0ef61889ef689d4
Ebony Butler <jusjctin222@gmail.com>
2022-05-23 12:57:09 -0700

    solution003

---
+++ js/solution003.js
@@ -0,0 +1,4 @@
+function goals (laLigaGoals, copaDelReyGoals, championsLeagueGoals) {
+    totalGoals = laLigaGoals + copaDelReyGoals + championsLeagueGoals
+    return totalGoals// code goes here
+  }
