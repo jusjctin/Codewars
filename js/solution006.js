a30468fe6adc38410277336680bf547c75a54882
Ebony Butler <jusjctin222@gmail.com>
2022-10-17 12:58:15 -0700

    solution006

---
+++ js/solution006.js
@@ -0,0 +1,10 @@
+function sum(numbers) { 
+  let sum = 0; // initialize sum 
+
+  // Iterate through all elements 
+  // and add them to sum 
+  for (let i = 0; i < numbers.length; i++) 
+      sum += numbers[i]; 
+
+  return sum; 
+} 
